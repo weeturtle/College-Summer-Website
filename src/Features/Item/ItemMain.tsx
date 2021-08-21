5946b95997040952020bacc87cdc2f439348fa02
weeturtle <grahamluk66@gmail.com>
2021-08-21 23:08:17 +0100

    Item Main Complete

--- src/Features/Item/ItemMain.tsx
+++ src/Features/Item/ItemMain.tsx
@@ -8,20 +8,55 @@ const ProductMainWrapper = styled.div`
 `
 
 const ProductInformation = styled.div`
-width: 30%;
-height: 62.5%;
-min-height: 25rem;
+width: 37.5rem;
+height: 22.5rem;
+position: absolute;
+display: inline-block;
+top: 5rem;
+right: 15%;
 `
 
-// const ProductImage = styled.image`
-// `
+const ProductImage = styled.img`
+height: 35rem;
+position: relative;
+top: 2rem;
+left: 5%;
+z-index: 2;
+display: inline-block;
+`
 
-const ProductStats = styled.div`
+const ProductStatsWrapper = styled.div`
 background-color: #FFF3E1;
 height: 10rem;
 width: 100%;
 position: absolute;
 bottom: 0;
+`
+
+const ProductStatsContainer = styled.div`
+width: 35%;
+height: 7.5rem;
+position: absolute;
+right: 11%;
+bottom: 12.5%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+const IndividualStat = styled.div`
+text-transform:capitalize;
+`
+
+const StatHeader = styled.h4`
+color: #535353;
+font-size: 1.5rem;
+`
+
+const StatInfo = styled.h4`
+color: #607D47;
+font-size: 1.75rem;
+margin-top: 1rem;
 `
 
 const ProductTitle = styled.h2`
@@ -32,11 +67,13 @@ color: #607D47;
 const ProductOrigin = styled.h3`
 font-size: 2.5rem;
 color: #535353;
+padding-top: 0.85rem;
 `
 
 const ProductDescription = styled.p`
 font-size: 1.5rem;
 color: #535353;
+padding-top: 0.85rem;
 `
 
 const CartSection = styled.div`
@@ -44,6 +81,8 @@ display: flex;
 width: 100%;
 justify-content: space-between;
 align-items: center;
+position: absolute;
+bottom: 0;
 `
 
 interface props {
@@ -57,6 +96,7 @@ export const ItemMain: React.FC<props> = ({item}) => {
     
     return (
         <ProductMainWrapper>
+            <ProductImage src={require(`../../assets/ProductImages/${item.image}`).default} />
             <ProductInformation>
                 <ProductTitle>{item.name}</ProductTitle>
                 <ProductOrigin>{item.location}</ProductOrigin>
@@ -66,9 +106,22 @@ export const ItemMain: React.FC<props> = ({item}) => {
                     <DetailsButton itemId={item.id}/>
                 </CartSection>
             </ProductInformation>
-            <ProductStats>
-
-            </ProductStats>
+            <ProductStatsWrapper>
+                <ProductStatsContainer>
+                    <IndividualStat>
+                        <StatHeader>Origin</StatHeader>
+                        <StatInfo>{item.location}</StatInfo>
+                    </IndividualStat>
+                    <IndividualStat>
+                        <StatHeader>Flavour</StatHeader>
+                        <StatInfo>{item.notes[0]} and {item.notes[1]}</StatInfo>
+                    </IndividualStat>
+                    <IndividualStat>
+                        <StatHeader>Roast</StatHeader>
+                        <StatInfo>{item.roast}</StatInfo>
+                    </IndividualStat>
+                </ProductStatsContainer>
+            </ProductStatsWrapper>
         </ProductMainWrapper>
     )
 }
